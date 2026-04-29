24c0ac141621e479b1772ea24da00c48e7ca1a3c
Victoria1-bit <olabodevictoria02@gmail.com>
2026-04-30 00:11:10 +0100

    Progress: Board loading implemented, file path issue to fix next

--- main.cpp
+++ main.cpp
@@ -1,38 +1,22 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
-#include "Crawler.h"
-#include "Hopper.h"
+#include "Board.h"
 
 using namespace std;
 
 int main() {
 
-    // Seed random number generator
-    srand(time(0));
+    cout << "Bug Board Test" << endl;
 
-    cout << "Bug Movement Test" << endl;
+    // Create board object
+    Board board;
 
-    // Create a crawler
-    // ID 101, position (0,0), facing East, health 10
-    Crawler crawler1(101, 0, 0, EAST, 10);
+    // Load bugs from file (FIXED PATH)
+    board.loadBugsFromFile("../bugs.txt");
 
-    // Create a hopper
-    // ID 102, position (2,2), facing South, health 8, hop length 3
-    Hopper hopper1(102, 2, 2, SOUTH, 8, 3);
+    cout << "\nAll Bugs:" << endl;
 
-    cout << "\nBefore move:" << endl;
-    crawler1.display();
-    hopper1.display();
-
-    // Move both bugs once
-    crawler1.move();
-    hopper1.move();
-
-    cout << "\nAfter move:" << endl;
-    crawler1.display();
-    hopper1.display();
+    // Display all bugs
+    board.displayAllBugs();
 
     return 0;
 }
