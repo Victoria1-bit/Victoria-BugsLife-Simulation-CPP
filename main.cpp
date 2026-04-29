acb4b7ee7bcf1849aa726e00d63b102cc00e2867
Victoria1-bit <olabodevictoria02@gmail.com>
2026-04-29 23:48:11 +0100

    Add commented test for Crawler movement

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Crawler.h"
+
 using namespace std;
 
 int main() {
-    cout << "Bug Simulation Project Started" << endl;
+
+    // Seed random number generator (used for direction changes)
+    srand(time(0));
+
+    cout << "Crawler Test" << endl;
+
+    // Create a crawler bug
+    // ID = 101, starting at (0,0), facing EAST, health = 10
+    Crawler crawler1(101, 0, 0, EAST, 10);
+
+    // Display initial state
+    cout << "Before move:" << endl;
+    crawler1.display();
+
+    // Move the crawler once
+    crawler1.move();
+
+    // Display state after moving
+    cout << "After move:" << endl;
+    crawler1.display();
+
     return 0;
 }
