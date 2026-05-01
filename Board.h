ecac34d74f9f9fae698e42313fedbfc46d2b98c0
Victoria1-bit <olabodevictoria02@gmail.com>
2026-05-01 14:37:07 +0100

    Add tapBoard function declaration

--- Board.h
+++ Board.h
@@ -2,17 +2,29 @@
 #define BOARD_H
 
 #include <vector>
+#include <string>
 #include "Bug.h"
 
 class Board {
 private:
-    std::vector<Bug*> bugs; // stores all bugs
+    // Vector stores pointers to all bugs
+    std::vector<Bug*> bugs;
 
 public:
+    // Load bugs from bugs.txt file
     void loadBugsFromFile(const std::string& filename);
+
+    // Display all bugs
     void displayAllBugs() const;
 
-    ~Board(); // destructor to clean memory
+    // Find one bug by ID
+    void findBug(int id) const;
+
+    // Move all bugs once
+    void tapBoard();
+
+    // Destructor clears heap memory
+    ~Board();
 };
 
 #endif
