92b38a3d6c41b9cd9a781a2239d9453661f1efdf
Victoria1-bit <olabodevictoria02@gmail.com>
2026-05-07 18:47:09 +0100

    Add DiagonalBug class with diagonal movement

---
+++ DiagonalBug.cpp
@@ -0,0 +1,53 @@
+#include "DiagonalBug.h"
+#include <iostream>
+#include <cstdlib>
+
+DiagonalBug::DiagonalBug(int id, int x, int y, Direction dir, int health)
+        : Bug(id, x, y, dir, health) {
+}
+
+// DiagonalBug moves diagonally by 1 square
+void DiagonalBug::move() {
+    if (!alive) {
+        return;
+    }
+
+    // If it is at an edge, choose a new direction
+    while (isWayBlocked()) {
+        direction = static_cast<Direction>((rand() % 4) + 1);
+    }
+
+    if (direction == NORTH) {
+        position.first++;
+        position.second--;
+    }
+    else if (direction == EAST) {
+        position.first++;
+        position.second++;
+    }
+    else if (direction == SOUTH) {
+        position.first--;
+        position.second++;
+    }
+    else if (direction == WEST) {
+        position.first--;
+        position.second--;
+    }
+
+    // Keep bug inside the 10x10 board
+    if (position.first < 0) position.first = 0;
+    if (position.first > 9) position.first = 9;
+    if (position.second < 0) position.second = 0;
+    if (position.second > 9) position.second = 9;
+
+    path.push_back(position);
+}
+
+void DiagonalBug::display() const {
+    std::cout << "Bug " << id
+              << " DiagonalBug"
+              << " | Pos: (" << position.first << "," << position.second << ")"
+              << " | HP: " << health
+              << " | " << (alive ? "Alive" : "Dead")
+              << std::endl;
+}
